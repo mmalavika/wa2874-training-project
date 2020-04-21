b138d02fea6a133d5d81ee07c4e0f0f6dd469d70
Malavika <mmalavika@alumni.ubc.ca>
2020-04-21 15:19:09 -0400

    Initial customer api commit

---
+++ customer-api/src/main/java/com/webage/spring/customer/CustomerApi.java
@@ -0,0 +1,14 @@
+package com.webage.spring.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApi.class, args);
+
+	}
+
+}
---
+++ customer-api/src/main/java/com/webage/spring/customer/domain/Customer.java
@@ -0,0 +1,5 @@
+package com.webage.spring.customer.domain;
+
+public class Customer {
+	//TODO customer model object
+}
